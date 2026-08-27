ad893267410fa9f02a4079d12ff5df18cc60cb86
aaaon <aonzxan47@gmail.com>
2026-08-27 11:22:40 +0700

    Refactor(ASP.NET): แปลงโปรเจกต์จาก MVC เป็น Web API และเปิด Swagger

--- TempWatch2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TempWatch2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
